990eae476a857aef86b6659e38e88c920f51d595
Tony <34975907+AnonymousWalker@users.noreply.github.com>
2019-11-10 18:36:37 -0600

    client-thread + refactored

--- user-client.py
+++ user-client.py
@@ -1,23 +1,38 @@
 from socket import *
-i = 50
+from _thread import *
+i = 20
 username = input(str("Please enter your username: "))
-messageContent = "I'm texting message body here..."
+#messageContent = "I'm texting message body here..."
 
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect(('HoangAnh',5555))
-msg = clientSocket.recv(1024).decode()
+msg = clientSocket.recv(1024).decode()  #welcome message from server
 print(msg)
 
-while (i > 0):
-    i = i - 1
-    try:
-        #send a message
-        msgheader = "UNameL: 2\r\nMessageL: 4\r\nUsername: " + username + "\r\nMessage: " + messageContent + "\r\n"
-        print("Me: "+ messageContent)
-        clientSocket.send(msgheader.encode())
+def sendMessage():
+    while (i > 0):
+        i = i - 1
+        try:
+            # send a message
+            messageToSend = input(str("Please enter your username: "))
+            print("Me: " + messageToSend)
+            msgheader = "UNameL: 2\r\nMessageL: 4\r\nUsername: " + username + "\r\nMessage: " + messageToSend + "\r\n"
+            clientSocket.send(msgheader.encode())
+        except IOError:
+            i=0
+            clientSocket.close()
 
-        # extract message received from server
-        msg = clientSocket.recv(1024).decode() #wait for other message
-        print("Someone's message: " + msg)
-    except IOError:
-        clientSocket.close()
+def receiveMessage():
+    while (i>0):
+        try:
+            message = clientSocket.recv(1024).decode()  # wait for other message
+            message.split('\r\n')
+            username = message[2].split()[1]
+            messageContent = message[3][9:]
+            print(username +": "+ messageContent)
+        except IOError:
+            clientSocket.close()
+            break;
+
+start_new_thread(receiveMessage)
+start_new_thread(sendMessage)
