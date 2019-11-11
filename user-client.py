53f2d9a3b86a84700cb6d7ab3cc881f2a9255034
Tony <34975907+AnonymousWalker@users.noreply.github.com>
2019-11-10 18:36:10 -0600

    test client

--- user-client.py
+++ user-client.py
@@ -1,18 +1,23 @@
 from socket import *
 i = 50
-clientname = 'anonymous_client'
+username = input(str("Please enter your username: "))
 messageContent = "I'm texting message body here..."
+
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect(('HoangAnh',5555))
 msg = clientSocket.recv(1024).decode()
 print(msg)
+
 while (i > 0):
     i = i - 1
     try:
-        msgheader = "UNameL: 2\r\nMessageL: 4\r\nUsername: " + clientname + "\r\nMessage: "+messageContent+"\r\n"
+        #send a message
+        msgheader = "UNameL: 2\r\nMessageL: 4\r\nUsername: " + username + "\r\nMessage: " + messageContent + "\r\n"
         print("Me: "+ messageContent)
         clientSocket.send(msgheader.encode())
+
+        # extract message received from server
         msg = clientSocket.recv(1024).decode() #wait for other message
-        print(msg)
+        print("Someone's message: " + msg)
     except IOError:
         clientSocket.close()
