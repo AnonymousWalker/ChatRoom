0c2be0807a37ba713a05b973e6cd00d093eabe7c
Tony <34975907+AnonymousWalker@users.noreply.github.com>
2019-11-12 15:19:44 -0600

    allow user enter IP, port

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from socket import *
 from _thread import *
+from threading import *
 
 serverSocket = socket(AF_INET, SOCK_STREAM)
 
@@ -7,41 +8,56 @@ serverPort = 5555
 serverAddress = gethostbyname(gethostname())    #server ip
 
 serverSocket.bind((serverAddress, serverPort))
-serverSocket.listen(20)     #number of connections are allowed, enabled phase
+serverSocket.listen(1)     #number of connections are allowed, enabled phase
 
-clientList = []     #list of client (address,username)
+clientList = {}     #list of client (address,username)
 
 #thread for handling one user connection
-def userThread(connection, address):
-    connection.send(("Welcome "+ address[0] +", this is the public chatroom!").encode())
+def userThread(connection, address, username):
+    connection.send(("Welcome "+ username +", this is the public chatroom. Use '/signout' to exit. Type your message below\r\n------------").encode())
+    cnnNotification = username+" ("+address[0]+") has joined the chat room"
+    print(cnnNotification)
+    publish(cnnNotification, connection, "[system]")
     while True:
         try:
+            #waiting for client message
             message = connection.recv(1024).decode()
             print(message)
             if message:
                 #extract message data
                 message = message.split('\r\n')
-                username = message[2].split()[1]
-                messageContent = message[3][9:]
-                publish(messageContent, username)
-        except IOError: #remove user from chat room if there is an error/timeout
+                senderName = message[2].split()[1]
+                messageContent = message[3].split(' ',1)[1]
+                publish(messageContent, connection, senderName)
+        except IOError:
+            #remove user from chat room if there is an error/timeout
             if connection in clientList:
-                clientList.close()
+                disconnectMsg = clientList[connection] + " has disconnected!"
+                print(disconnectMsg)
+                publish(disconnectMsg, connection, "[system]")
+                connection.close()
+                del clientList[connection]
 
-def publish(message, connection):
+
+def publish(message, connection, username):
     for user in clientList:
-        # if user != connection:
-        if True:
+        if user != connection:
+        #if True:
             try:
-                msgheader = "UNameL: 2\r\nMessageL: 4\r\nUsername: "+ connection +"\r\nMessage: "+ message +"\r\n"
+                msgheader = "UNameL: 2\r\nMessageL: 4\r\nUsername: "+ username +"\r\nMessage: "+ message +"\r\n"
                 user.send(msgheader.encode())
             except IOError:
-                # user.close()
-                clientList.remove(connection)
+                #clientList.remove(connection)
+                break
+
+def sizeof(data):
+    return len(data.encode('utf-8'))
 
 #server establish new incoming connections
 while True:
     connection, cAddress = serverSocket.accept()
-    clientList.append(connection) #add new connection list
-    start_new_thread(userThread,(connection,cAddress)) #create a new thread for each arriving connection
-
+    username = connection.recv(20).decode()
+    #clientList.append(connection) #add new connection list
+    clientList[connection] = username
+    #start_new_thread(userThread,(connection,cAddress)) #create a new thread for each arriving connection
+    Thread(target=userThread, args=(connection, cAddress, username)).start()
--- user-client.py
+++ user-client.py
@@ -1,30 +1,111 @@
 from socket import *
-import sys
-import select
-i = 20
-username = input(str("Please enter your username: "))
-#messageContent = "I'm texting message body here..."
+from threading import *
+import os
 
+
+def sizeof(data):
+    return len(data.encode('utf-8'))
+
+
+# thread to send msg
+def sendMessage(sock):
+    while True:
+        messageToSend = input()
+        if messageToSend == "/signout":
+            sock.close()
+            os._exit(1)
+        msgLen = sizeof(messageToSend)
+        if msgLen > 5: #msg max length is 4 bytes 4,294,967,296
+            print("(Error!) Your message is too long, plesase try again!")
+            continue
+        msgheader = "UNameL: "+str(uNameLen)+"\r\nMessageL: " + str(msgLen) + "\r\nUsername: " + username + "\r\nMessage: " + messageToSend + "\r\n"
+        sock.send(msgheader.encode())
+
+
+# thread to recv msg
+def receiveMessage(sock):
+    while True:
+        message = sock.recv(1024).decode()  # wait for other message
+        message = message.split('\r\n')
+        senderName = message[2].split()[1]
+        messageContent = message[3].split(' ', 1)[1]
+        if senderName == '[system]':
+            print("---" + messageContent + "---")
+        else:
+            print("<" + senderName + ">\t" + messageContent)
+
+
+username = input("Please enter your username: ")
+uNameLen = sizeof(username)
+#Validate uname length: 2 bytes
+while uNameLen > 5: #65535:
+    username = input("Username is too long, please try again: ")
+    uNameLen = sizeof(username)
 clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect(('HoangAnh',5555))
-msg = clientSocket.recv(1024).decode()  #welcome message from server
+host = input("Enter the server address to connect: ")
+port = input("Enter server port number: ")
+clientSocket.connect((host, port))
+clientSocket.send(username.encode())
+msg = clientSocket.recv(1024).decode()  # welcome message from server
 print(msg)
 
-while True:
-    try:
-        sockets = [sys.stdin, clientSocket]
-        rsockets, wsockets, esockets =  select.select(sockets, [], [])
-        for socket in rsockets:
-            if socket == clientSocket:
-            # extract message received from server
-                msg = clientSocket.recv(1024).decode() #wait for other message
-                print("Someone's message: " + msg)
-            else:
-            # send a message
-                messageToSend = input(str("Enter your message: "))
-                print("Me: " + messageToSend)
-                msgheader = "UNameL: 2\r\nMessageL: 4\r\nUsername: " + username + "\r\nMessage: " + messageToSend + "\r\n"
-                clientSocket.send(msgheader.encode())
+Thread(target=sendMessage, args=(clientSocket,)).start()
+Thread(target=receiveMessage, args=(clientSocket,)).start()
 
-    except IOError:
-        clientSocket.close()
+# def sendMessage(sock):
+#     i=10
+#     while (i > 0):
+#         i = i - 1
+#         try:
+#             # send a message
+#             messageToSend = input(str("Enter your message: "))
+#             print("Me: " + messageToSend)
+#             msgheader = "UNameL: 2\r\nMessageL: 4\r\nUsername: " + username + "\r\nMessage: " + messageToSend + "\r\n"
+#             sock.send(msgheader.encode())
+#         except IOError:
+#             i=0
+#             break
+#             #clientSocket.close()
+#
+# def receiveMessage(sock):
+#     i = 10
+#     while (i>0):
+#         try:
+#             message = sock.recv(1024).decode()  # wait for other message
+#             message.split('\r\n')
+#             username = message[2].split()[1]
+#             messageContent = message[3][9:]
+#             print(username +": "+ messageContent)
+#         except IOError:
+#             i=0
+#             break;
+#
+#
+# start_new_thread(sendMessage, (clientSocket,))
+# start_new_thread(receiveMessage, (clientSocket,))
+#
+#
+
+
+# while i:
+#     i -=1
+#     try:
+#         socketList = [sys.stdin, clientSocket]
+#         #rsockets, wsockets, esockets = select.select(socketList, [], [])
+#         read_sockets, write_socket, error_socket = select.select(socketList, [], [])
+#         for s in read_sockets:
+#             if s == clientSocket:
+#             # extract message received from server
+#                 msg = clientSocket.recv(1024).decode() #wait for other message
+#                 print("Someone's message: " + msg)
+#                 sys.stdout.flush()
+#             else:
+#             # send a message
+#                 messageToSend = input(str("Enter your message: "))
+#                 print("Me: " + messageToSend)
+#                 sys.stdout.flush()
+#                 msgheader = "UNameL: 2\r\nMessageL: 4\r\nUsername: " + username + "\r\nMessage: " + messageToSend + "\r\n"
+#                 clientSocket.send(msgheader.encode())
+#
+#     except IOError:
+#         clientSocket.close()
