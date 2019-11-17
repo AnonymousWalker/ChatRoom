09961d172ee29639c20bef1566b12456a77bee11
Hoang Anh <hoanganhtran1998@gmail.com>
2019-11-17 17:18:49 -0500

    successfully estab pm by connect/accept

--- server.py
+++ server.py
@@ -5,18 +5,20 @@ from threading import *
 CONNECTION_LIMIT = 10
 serverSocket = socket(AF_INET, SOCK_STREAM)
 serverPort = 5555
-serverAddress = gethostbyname(gethostname())    #server ip
+serverAddress = gethostbyname(gethostname())  # server ip
 
 serverSocket.bind((serverAddress, serverPort))
 serverSocket.listen(20)
 
-clientList = {}     #list of client (address,username)
+clientList = {}  # list of client (address,username)
 
-#thread for one user connection
-def userThread(connection, address, username):
+
+# thread for one user connection
+def userThread(connection, address, senderName):
+    pmUser = None
     while True:
         try:
-            #waiting for client message
+            # waiting for client message
             message = connection.recv(1024).decode()
             print(message)
             if message:
@@ -24,34 +26,63 @@ def userThread(connection, address, username):
 
                 if msgType == 'cmd':
                     if messageContent == '/join':
-                        connection.send(formatMessage('[system]', "You have joined the public chatroom. Use '/signout' to exit. Type your message below\r\n").encode())
-                        notification = username + " (" + address[0] + ") has joined the chat room"
+                        connection.send(formatMessage('[system]',
+                                                      "You have joined the public chatroom. Use '/signout' to exit. "
+                                                      "Type your message below\r\n").encode())
+                        notification = senderName + " (" + address[0] + ") has joined the chat room"
                         print(notification)
                         publish(notification, connection, "[system]")
 
-                    elif messageContent == '/fetch':        #display active users
+                    elif messageContent == '/fetch':  # display active users
                         activeList = "Active users:\r\n"
                         for user in clientList:
-                            activeList += clientList[user]['username'] +" ("+ clientList[user]['ip']+"/"+ str(clientList[user]['port'])+")\r\n"
+                            activeList += clientList[user]['username'] + " (" + clientList[user]['ip'] + "/" + str(
+                                clientList[user]['port']) + ")\r\n"
                         connection.send(formatMessage('[system]', activeList).encode())
 
-                    elif messageContent.startswith('/connect'):     #forward invitation
+                    elif messageContent.startswith('/connect'):  # forward invitation
                         messageContent = messageContent[1:].split('/')
                         ip = messageContent[1]
                         port = messageContent[2]
 
-                        #find client in list
+                        # find receiver in list
                         for conn in clientList:
+                            #matching receiver
                             if clientList[conn]['ip'] == ip and clientList[conn]['port'] == int(port):
-                                forwdMsg = formatMessage('[system]', username+"("+ ip +"/"+ port +") invited you to private message.\r\nType '/connect/ip/port' to accept.")
+                                pmUser = (conn, clientList[conn]['username'])
+                                forwdMsg = formatMessage('[system]',
+                                                         senderName + "(" + address[0] + "/" + str(address[1]) + ") invited you to private "
+                                                                                            "message.\r\nType "
+                                                                                            "'/accept/ip/port' to accept.")
                                 conn.send(forwdMsg.encode())
                                 break
-                        print(messageContent)
+
+                    elif messageContent.startswith('/accept'):
+                        acceptMsg = formatMessage(username, messageContent)
+                        messageContent = messageContent[1:].split('/')
+                        ip = messageContent[1]
+                        port = messageContent[2]
+
+                        # find inviter
+                        for conn in clientList:
+                            if clientList[conn]['ip'] == ip and clientList[conn]['port'] == int(port):
+                                pmUser = (conn, clientList[conn]['username'])
+                                forwdMsg = formatMessage('[system]', '/accept', 'cmd')
+                                conn.send(forwdMsg.encode())
+                                break
+
+                elif pmUser:
+                    try:
+                        pm = formatMessage(senderName, messageContent, 'pm')
+                        pmUser[0].send(pm.encode())
+                    except IOError:
+                        pmUser = None
+                        print(senderName+" couldn't connect to "+ pmUser[1])
                 else:
                     publish(messageContent, connection, senderName)
 
         except IOError:
-            #remove user from chat room if there is an error/timeout
+            # remove user from chat room if there is an error/timeout
             if connection in clientList:
                 disconnectMsg = clientList[connection]["username"] + " has disconnected!"
                 print(disconnectMsg)
@@ -63,7 +94,7 @@ def userThread(connection, address, username):
 def publish(message, connection, username):
     for user in clientList:
         if user != connection:
-        #if True:
+            # if True:
             try:
                 msgheader = formatMessage(username, message)
                 user.send(msgheader.encode())
@@ -71,36 +102,39 @@ def publish(message, connection, username):
                 if user in clientList.keys():
                     del clientList[user]
 
+
 def formatMessage(username, message, msgType="msg"):
-    return "UNameL: "+ str(len(username)) +"\r\nMessageL: "+ str(len(message)) +"\r\nMessageType: "+ msgType +"\r\nUsername: " + username + "\r\nMessage: " + message
+    return "UNameL: " + str(len(username)) + "\r\nMessageL: " + str(
+        len(message)) + "\r\nMessageType: " + msgType + "\r\nUsername: " + username + "\r\nMessage: " + message
+
 
 def extractMsgHeader(message):
-    message = message.split('\r\n',4)
+    message = message.split('\r\n', 4)
     username = message[3].split()[1]
     messageType = message[2].split()[1]
-    messageContent = message[4].split(' ',1)[1]
+    messageContent = message[4].split(' ', 1)[1]
     return (username, messageType, messageContent)
 
-#server establish new incoming connections
+
+# server establish new incoming connections
 while True:
     connection, cAddress = serverSocket.accept()
 
-    #check room capacity
+    # check room capacity
     if len(clientList) >= CONNECTION_LIMIT:
         msg = "Server is full at the moment, please try again later!"
         connection.send(format("[server]", msg).encode())
         connection.close()
         continue
 
-    unameMsg = connection.recv(1024).decode()     #recv username
+    unameMsg = connection.recv(1024).decode()  # recv username
     username = unameMsg.split('\r\n')[3].split()[1]
     # command = connection.recv(1024).decode()
     # print(command)
 
-    # welcomeMsg = "Welcome "+ username +", this is the public chatroom. Use '/signout' to exit. Type your message below\r\n------------"
-    welcomeMsg = "Welcome to chat server "+ gethostname()
+    welcomeMsg = "Welcome to chat server " + gethostname()
     connection.send(formatMessage("[server]", welcomeMsg).encode())
 
     clientList[connection] = {"username": username, "ip": cAddress[0], "port": cAddress[1]}
-    #create a new thread for each arriving connection
+    # create a new thread for each arriving connection
     Thread(target=userThread, args=(connection, cAddress, username)).start()
--- user-client.py
+++ user-client.py
@@ -10,7 +10,7 @@ def sendMessage(sock):
         if messageToSend == "/signout":
             sock.close()
             os._exit(1)
-        if len(messageToSend) > 4294967295: #msg max length is 4 bytes = 4,294,967,296
+        if len(messageToSend) > 4294967295:  # msg max length is 4 bytes = 4,294,967,296
             print("(Error!) Your message is too long, plesase try again!")
             continue
         msgheader = formatMessage(username, messageToSend, 'cmd' if messageToSend.startswith('/') else 'msg')
@@ -34,52 +34,58 @@ def receiveMessage(sock):
 
 
 def formatMessage(username, message, msgType="msg"):
-    return "UNameL: "+ str(len(username)) +"\r\nMessageL: "+ str(len(message)) +"\r\nMessageType: "+ msgType +"\r\nUsername: " + username + "\r\nMessage: " + message
+    return "UNameL: " + str(len(username)) + "\r\nMessageL: " + str(
+        len(message)) + "\r\nMessageType: " + msgType + "\r\nUsername: " + username + "\r\nMessage: " + message
+
 
 def extractMsgHeader(message):
-    message = message.split('\r\n',4)
+    message = message.split('\r\n', 4)
     username = message[3].split()[1]
     messageType = message[2].split()[1]
-    messageContent = message[4].split(' ',1)[1]
+    messageContent = message[4].split(' ', 1)[1]
     return (username, messageType, messageContent)
 
 
 username = input("Please enter your username: ")
-#Validate uname length: 2 bytes = 2^16 MAX
+# Validate uname length: 2 bytes = 2^16 MAX
 while len(username) > 65535:
     username = input("Username is too long, please try again: ")
 clientSocket = socket(AF_INET, SOCK_STREAM)
 
 host = input("Enter the server address to connect: ")
 port = input("Enter server port number: ")
+os.system('cls')
 clientSocket.connect((host, int(port)))
 
 unameMsg = formatMessage(username, username, 'cmd')
 clientSocket.send(unameMsg.encode())
 
 msg = clientSocket.recv(1024).decode()  # welcome message from server
-welcomeMsg = msg.split('\r\n',4)[4].split(' ', 1)[1]
+welcomeMsg = msg.split('\r\n', 4)[4].split(' ', 1)[1]
 print(welcomeMsg)
+print(
+    '>> Use the following command:\r\n\t/join : access public chatroom\r\n\t/fetch : see active users\r\n\t/connect/ip/port : invite user to private message\r\n\t/exit : close application.\r\n')
 
 while True:
-    cmd = input('>> Use the following command:\r\n\t/join : access public chatroom\r\n\t/fetch : see active users\r\n\t/connect/ip/port : invite user to private message\r\n\t/exit : close application.\r\n')
-
-    if cmd == '/exit':
-        clientSocket.close()
-        os._exit(1)
-    elif cmd.startswith('/'):
+    cmd = input()
+    if cmd.startswith('/'):
+        if cmd == '/exit':
+            clientSocket.close()
+            os._exit(1)
         msg = formatMessage(username, cmd, "cmd")
-        clientSocket.send(msg.encode())     #send command
-        if True: #cmd == '/join':
-            response = clientSocket.recv(1024).decode()  # recv server response
-            sender, msgType, msgContent = extractMsgHeader(response)
-            print(msgContent)
+        clientSocket.send(msg.encode())  # send command
+
+        response = clientSocket.recv(1024).decode()  # recv server response
+        sender, msgType, msgContent = extractMsgHeader(response)
+        print(msgContent)
+        if cmd == '/join':
             break
+        # if cmd == '/fetch': #do nothing
 
+        if cmd.startswith('/connect'):
+            if msgContent.startswith('/accept'):  # start private message
+                break
 
-
-#join chatroom
+# join chatroom
 Thread(target=sendMessage, args=(clientSocket,)).start()
 Thread(target=receiveMessage, args=(clientSocket,)).start()
-
-
